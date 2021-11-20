513af309169d71da40dc335bd57bbb36c3e613dc
rewind98 <quanvapk01708@fpt.edu.vn>
2021-11-20 07:39:54 +0700

    login first commit

--- app/src/main/java/com/example/myapplication/Activities/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/LoginActivity.java
@@ -1,4 +1,26 @@
 package com.example.myapplication.Activities;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+    Button BTN_login_DangNhap, BTN_login_DangKy;
+    TextInputLayout TXTL_login_TenDN, TXTL_login_MatKhau;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        //thuộc tính view
+        TXTL_login_TenDN = (TextInputLayout)findViewById(R.id.txtl_login_TenDN);
+        TXTL_login_MatKhau = (TextInputLayout)findViewById(R.id.txtl_login_MatKhau);
+        BTN_login_DangNhap = (Button)findViewById(R.id.btn_login_DangNhap);
+        BTN_login_DangKy = (Button)findViewById(R.id.btn_login_DangKy);
+    }
 }
